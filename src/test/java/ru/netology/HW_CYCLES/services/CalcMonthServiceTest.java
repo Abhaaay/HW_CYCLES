7432db86c544886e4e930960b3c6f688c320e02b
Abhay <kiyashkoandrey1988@gmail.com>
2023-06-15 14:35:04 +0300

    Added partest Csvfilesource

--- src/test/java/ru/netology/HW_CYCLES/services/CalcMonthServiceTest.java
+++ src/test/java/ru/netology/HW_CYCLES/services/CalcMonthServiceTest.java
@@ -1,15 +1,18 @@
 package ru.netology.HW_CYCLES.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalcMonthServiceTest {
-    @Test
-    public void testCalculateMonthsOfRest() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Data.csv")
+    public void testCalculateMonthsOfRest(int income, int expenses, int threshold,int expectedMonthsOfRest) {
         CalcMonthService service = new CalcMonthService();
-        int income = 10000; // доход от работы
-        int expenses = 3000; // обязательные месячные траты
-        int threshold = 20000; // пороговое значение денег на счету
-        int expectedMonthsOfRest = 3; // ожидаемое количество месяцев отдыха
+        //int income = 10000; // доход от работы
+        //int expenses = 3000; // обязательные месячные траты
+        //int threshold = 20000; // пороговое значение денег на счету
+        //int expectedMonthsOfRest = 3; // ожидаемое количество месяцев отдыха
         int actualMonthsOfRest = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expectedMonthsOfRest, actualMonthsOfRest);
